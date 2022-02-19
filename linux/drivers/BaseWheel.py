b9be3ea3fae0d5130032b85b13020e45efe85d4b
TomCC7 <playercc7@gmail.com>
2022-02-19 01:02:03 -0500

    Change structure of wheels drivers
    1. Move event handlers to BaseWheel since many of them are shared
    2. Complete event handlers
    3. change ev variables to static

--- linux/drivers/BaseWheel.py
+++ linux/drivers/BaseWheel.py
@@ -1,25 +1,22 @@
 #!/usr/bin/env python
-import evdev
-import asyncio
-from evdev import ecodes, InputDevice, ff
+from evdev import ecodes
 import math
-from linux import config
 from abc import ABC
 
 class BaseWheel(ABC):
     """
     Abstract wheel class to be inherited
     """
+    # static variables
+    ev_key_event:dict = {}
+    ev_abs_event:dict = {}
+    ev_type_accepted:tuple = (1,3)
+    ev_events:list = [None,
+                      ev_key_event,
+                      None,
+                      ev_abs_event]
     def __init__(self):
         # register events
-        # TODO: register keys
-        self.ev_key_event:dict = {}
-        self.ev_abs_event:dict = {}
-        self.ev_events:list = [None,
-                          self.ev_key_event,
-                          None,
-                          self.ev_abs_event]
-        self.ev_type_accepted:tuple = (1,3)
         # evdev device
         self.ev = None
         self.ev_name:str = ""
@@ -49,15 +46,17 @@ class BaseWheel(ABC):
         self.__setFF(ecodes.FF_AUTOCENTER, autocenterCmd)
 
 
-    async def events_handler(self) -> int:
+    async def events_handler(self) -> None:
         '''
         Capture and handle events using asyncio
         '''
         async for event in self.ev.async_read_loop():
             try:
-                self.ev_events[event.]
-
-        return 0
+                if event.type in self.ev_type_accepted:
+                    self.ev_events[event.type][event.code](event)
+            except:
+                raise Exception(
+                    "No handler for code {0} in type {1}".format(event.code, event.type))
 
     def __setFF(self,ff_type:int, val:float) -> None:
         """
@@ -75,6 +74,95 @@ class BaseWheel(ABC):
         """
         assert(val >= 0 and val <= 1)
         self.ev.write(ecodes.EV_FF, ff_type, int(65535*val))
+
+
+    def __key_xbox_handler(self,event):
+        pass
+
+
+    def __key_lsb_handler(self,event):
+        pass
+
+
+    def __key_rsb_handler(self,event):
+        pass
+
+
+    def __key_view_handler(self,event):
+        pass
+
+
+    def __key_menu_handler(self,event):
+        pass
+
+
+    def __key_a_handler(self,event):
+        pass
+
+
+    def __key_b_handler(self,event):
+        pass
+
+
+    def __key_x_handler(self,event):
+        pass
+
+
+    def __key_y_handler(self,event):
+        pass
+
+
+    def __key_lshift_handler(self,event):
+        """
+        handler for left paddle shift on G920
+        """
+        pass
+
+
+    def __key_rshift_handler(self,event):
+        """
+        handler for right paddle shift on G920
+        """
+        pass
+
+
+    def __abs__steer_handler(self,event):
+        """
+        handler for the steering wheel input on G920
+        """
+        pass
+
+
+    def __abs_acc_handler(self,event):
+        """
+        handler for the accelerator input
+        """
+        pass
+
+
+    def __abs_brake_handler(self,event):
+        """
+        handler for the brake input
+        """
+        pass
+
+
+    def __abs_clutch_handler(self,event):
+        """
+        handler for the clutch input
+        """
+        pass
+
+
+    def __abs_pad_handler(self,event):
+        """
+        handler for the directional pad
+        c:16 val:-1 -> left
+        c:16 val:1 -> right
+        c:17 val:-1 -> up
+        c:17 val:1 -> down
+        """
+        pass
 
 
     def __sync_event_handler(self, event):
