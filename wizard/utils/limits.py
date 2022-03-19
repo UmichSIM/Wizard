5eeb4a2d2ea307779b4e8d224f3ccd5193cf8dbb
cc <cctom@umich.edu>
2022-03-19 00:37:57 -0400

    Add utils/limits.py
    provides alias to easily get max and low values of different int types

---
+++ wizard/utils/limits.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Get the limit values
+"""
+
+import numpy as np
+iinfo = np.iinfo
+uint8 = np.uint8
+uint16 = np.uint16
+uint32 = np.uint32
+uint64 = np.uint64
+int16 = np.int16
+int32 = np.int32
+int64 = np.int64
