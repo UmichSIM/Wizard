62efabcc0a38538f9c439c9f157f6ff45f459a42
wanhanxi <wanhanxi@foxmail.com>
2022-10-02 15:25:55 -0400

    Add rumble teest

---
+++ wizard/carla_modules/RumbleStripSensor.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import carla
+import weakref
+from wizard.world import World
+from wizard.hud import HUD
+from wizard.carla_modules.vehicle import Vehicle
+
+class RumbleStripSensor(object):
+    """
+    Sensor to detect Rumble Strip.
+    It is done by detecting the 'LaneMarkingType' of the rumble strip.
+
+    alternative: detect 'LaneType', but it's not so accurate.
+    """
+    def __init__(self):
+        self.RStype = 'SolidSolid'
+        self.sensor = None
+        self._parent = Vehicle.get_instance().vehicle
+        self.hud = HUD.get_instance()
+        world = World.get_instance().world
+        bp = world.get_blueprint_library().find('sensor.other.lane_invasion')
+        self.sensor = world.spawn_actor(bp, carla.Transform(), attach_to=self._parent)
+        World.get_instance().register_death(self.sensor)
+        # We need to pass the lambda a weak reference to self to avoid circular
+        # reference.
+        weak_self = weakref.ref(self)
+        self.sensor.listen(lambda event: LaneInvasionSensor._on_invasion(weak_self, event))
+
+    @staticmethod
+    def _on_invasion(weak_self, event):
+        self = weak_self()
+        if not self:
+            return False
+        lane_types = set(x.type for x in event.crossed_lane_markings)
+
+        # debug
+        text = ['%r' % str(x).split()[-1] for x in lane_types]
+        self.hud.notification('RumbleStrip - Crossed line %s' % ' and '.join(text))
+
+        if self.RStype in lane_types:
+            return True
+        else:
+            return False
